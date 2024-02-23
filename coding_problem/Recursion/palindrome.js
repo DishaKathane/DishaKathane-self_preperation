0f3217b43180e1b89b5872fb5f5b9188da7c948f
dishakathane <dishakathane1995@gmail.com>
2024-02-23 23:59:32 +0530

    plaindrome

---
+++ coding_problem/Recursion/palindrome.js
@@ -0,0 +1,22 @@
+/**
+ * the given string is palindrome or not
+ * naman = reverse(naman) => naman is same as original string;
+ * abc =revesre(abc) =cba != palindrome
+ */
+
+function palindrome(str, left, right){
+if(str.length <= 1) return true;
+while(left<right){
+    if(str[left] === str[right]){
+        return palindrome(str,left+1, right-1);
+    }else{
+        return false;
+    }
+}
+return true;
+}
+console.log(palindrome("madam", 0, 4))
+let str = "naina";
+let left = 0;
+let right = str.length-1;
+console.log(palindrome(str,left,right))
