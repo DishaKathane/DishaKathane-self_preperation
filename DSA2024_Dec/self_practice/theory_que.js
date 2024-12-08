9f093fcf8c64f95dc87f5c00402ea4224031d5bf
dishakathane <dishakathane1995@gmail.com>
2024-12-08 15:58:09 +0530

    threory question added

---
+++ DSA2024_Dec/self_practice/theory_que.js
@@ -0,0 +1,200 @@
+/**
+ * Javascript
+1.	What is javascript? How does it work?
+2.	What is an asynchronous operation ? Is Javascript synchronous or asynchronous. 
+3.	Explain async using set timeout.
+4.	Is JS is a single threaded or multi threaded ?
+5.	Is JS an asynchronous or synchronous language ?
+6.	Difference between var let and const
+7.	What is hoisting 
+8.	What is a promise? What are the states of promises? What is promisify?
+9.	What is async await?
+10.	Difference between promise and async & await. Is async await notation preferred over promises? If so, why?
+11.	What are JavaScript Objects ? how to create them. 
+12.	How to search for a value or a key in an object? 
+13.	What is a callback function? 
+14.	What are the drawbacks of callback and what is callback hell.
+15.	 What is the difference between callback and promise. How can you write the same async operation using promise and callback 
+16.	Difference between function and arrow function
+17.	Difference between i++ and ++i
+18.	What is the “this” operator?
+19.	What is the closure concept?
+20.	what is closure - with code, why is it required, it's benefits 
+21.	What is json? What do the functions json.parse and json.stringify do?
+22.	What is the difference between '==' and '===' operators? 
+23.	What is the typeof operator? What is NaN?
+24.	What are settimeout and setinterval functions and what is the difference between them?
+25.	What is functional scope and block scope? 
+26.	What is shallowCopy and deepCopy
+27.	What is the difference between map and filter. Explain math , the built-in javascript object, functionalities. 
+28.	What is ES6? Explain some features of ES6
+29.	Difference btw typescript & javascript.
+30.	what is regex?
+31.	what is first class functions
+32.	diff between primitive and reference data types
+33.	Difference between null and undefined
+34.	difference between slice and splice 
+35.	what is spread operater and rest operator
+36.	how to convert intiger to string and string to integer
+37.	Explain JS Object methods like- Object.Keys,  Object.values and Object.entries
+
+
+API
+1.	What is an API? 
+2.	What are Rest Api?
+a.	What are rest Api ? why they are stateless?
+3.	What are the different ways to pass data to an API while requesting a Create operation? Discuss the Content-Type header in brief
+4.	Explain the GET, POST, PUT, POST and PATCH method types
+5.	DIfference between PUt and Patch
+6.	What is an HTTP status code? Discuss the popularly used codes along with their usecase.
+a.	What are http status codes? Please elaborate?
+
+
+Nodejs
+1.	What is Nodejs? What is the difference between Javascript and Nodejs? 
+2.	Node js is single threaded or multi threaded? Explain in detail what that means. 
+3.	What is meant by request and response cycle in a nodejs application?
+4.	What is express? What are the benefits of using this framework?
+5.	What is Middleware? What are global and route level middlewares?
+a.	Explain about application and route based middleware?
+6.	What is the function of an express router? What is a route and route splitting?
+7.	How Nodejs is different from a multithreaded architecture?
+8.	What is .env file or environment variables? Why are they important?
+9.	What is an event loop? Explain with an example.
+10.	security  -  how would you protect your node js server?
+
+
+
+MongodDb
+1.	Why is a database? Name a few examples.
+2.	Why you use MongoDB over MySQL and PostgreSQL?  MySql vs MongoDb ( or SQL/RDBMS vs NoSql). 
+3.	Compare relational and non-Relational databases and their benefits or drawbacks? 
+4.	What kind of db is MongoDb? Why do we define schema in nodejs application when working with Mongodb if it is schema-less?
+5.	What is an aggregation pipeline? What are some stages used in it?
+6.	What is a  Schema and a model in mongoose. How are they different?
+7.	Discuss some common mongoose APIs for CRUD operations.
+8.	Why do you have to use await if you want to process the result.
+9.	What are indexes?  What is the default index value set by MongoDB?
+a.	What do you mean by database indexing? 
+10.	How to link the document of one collection to a document in another collection using mongoose. Explain reference in mongoose.
+11.	What is the difference between find, findOne, findOneAndUpdate and findById APIs. 
+12.	Allso between updateOne, update and updateMany? When should you use each?
+
+
+AWS
+1.	What is AWS and AWS S3 ? 
+2.	What are the command to push in AWS S3?
+3.	What is meant by a bucket?
+4.	Can we upload image in database w/o using AWS S3. What are the benefits of using s3 instead?
+
+
+Caching
+	
+1.	What is meant by cache and caching?
+2.	Why we use redis 
+3.	Why do we need redis if we have databases?
+a.	Redis advantages and disadvantages.
+4.	Explain cache miss and cache hit.
+
+
+Auth
+1.	What is authentication and authorization? What is the difference between them?
+2.	What is jwt? Describe the parts of JWT? What are the benefits of using a JWT 
+a.	Why authorization is necessary?
+3.	How did you protected your Routes?
+
+Project
+1.	How do you implement authentication in your project?
+2.	 Explain the flow how a user is authenticated and the prerequisite for the same (token generation and sharing with the client)
+a.	How to generate a token ? How to verify it ?
+3.	Explain URL shortener project (explain the overview, features and how using redis). What is shortid. What happens if the same long url is passed in the request twice. How can you create a unique short code without using shortid package? How is the redirection happening?
+4.	Why do we use mvc (model view controller) in projects 
+5.	Deploy your project on Heroku ?
+
+Other (OOPS + Git + Multithreading)
+1.	OOPS overview.
+2.	What is inheritance and abstraction .
+3.	What is Git? 
+4.	What are some basic git commands?
+5.	What is a Git repository and branches? 
+6.	What is difference between Git and GitHub
+7.	What is class,id in html? What is DOM?
+Mock Interview Question.
+
+
+1.	Payment gateway of application.
+2.	How to restrict applications that can run on 1 or 2 devices only like Netflix, Hotstar.
+3.	How to handle time for discounts like flipkart.
+4.	What is an aggregation pipeline?
+5.	Name the internal node js library.
+6.	Difference between framework or library.
+7.	What is the Architecture of node js
+8.	What are the phases of the event loop?
+9.	Java script API methods
+10.	What is currying in js
+11.	Feature of strict mode.
+12.	What is stack and its main operation?
+13.	What is linkedList?
+14.	Time complexity of sorting linkedList.
+15.	What is a callback?
+16.	Binary search
+17.	How to handle that the user can login only from 1 device at a time.
+18.	Curing, aggregation term with 1 or 2 examples
+19.	Linked list concept
+20.	Mergesort concept only
+21.	Difference between framework and library.
+22.	How to handle a user's wrong input and make him know what the error is?
+23.	What to do if I want to make a separate file for errors?(answer : we can make a middleware for that) The next question arises from here: what is it and  where does it exist in api?
+24.	What is stack and queue….example where to use in tech.
+25.	nodejs ki working bhi puchhe the.... but question direct nhi tha .... tumko samjhna padega ki vo working of node js puchh rahe hai.
+26.	Time complexity of insertion and deletion in linked list
+27.	Path params and query params difference
+28.	Memory allocation in array vs linked list
+29.	How nodeJS handle concurrency
+30.	How event loop works
+31.	How did you handled error in projects
+32.	Database queries 
+33.	What we send in headers
+34.	What is nosql and difference between sql and nosql.
+35.	Why you have used mongoose package
+36.	What is global and route level middleware
+37.	Explain process of route handling 
+38.	Different type of http request
+39.	Construct binary tree from preorder,postorder and inorder data given
+40.	Which traversal in bst gives sorted list
+41.	Difference in stacks and queue
+42.	Difference  between single and doubly linked list
+43.	Why javascript is single threaded
+44.	How javascript behaves asynchronously
+45.	Which type of binary tree is similar to linked list(skew tree)
+46.	Why binary search time complexity is O(logn)
+47.	Difference between call,apply and bind method
+48.	Explain map, find and reduce .
+49.	What is currying also write code implementing it.
+50.	Promise.all()
+51.	Closures
+52.	Different types of sorting, how they all work, which one is better & why.
+53.	Deep copy and shallow copy
+JS BASICS
+1.	What is JavaScript?
+2.	List some features of JavaScript.
+3.	List some of the advantages of JavaScript.
+4.	What is the difference between JavaScript and JScript?
+5.	How to write a “Hello World” example of JavaScript?
+6.	How to use an external JavaScript file?
+7.	Is JavaScript case sensitive?
+8.	What is the use of window objects?
+9.	How to write multi line comments in JavaScript?
+10.	What are the different data types present in JavaScript?
+11.	What is the output of 10+20+ “30” and 10”+20+30 in JavaScript?
+12.	Difference between Client side JavaScript and Server side JavaScript?
+13.	What is this [[[]]]?
+14.	What are the pop-up boxes available in JavaScript?
+15.	What is the requirement of debugging in JavaScript?
+16.	What are the falsy values in JavaScript, and how can we check if a value is falsy?
+
+
+
+
+
+ */
